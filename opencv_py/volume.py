2d6058cf19ecd7217c61879e3562f64487bd6a00
sunjilei <sunjilei@test.com>
2021-02-08 10:53:38 +0800

    logistics package volume

--- opencv_py/volume.py
+++ opencv_py/volume.py
@@ -5,8 +5,10 @@ from util import plot_images
 
 
 def hsv_values():
-    files = ['009','010','011','b','g','r','black']
-    names = ['box','green','red','b','g','r','black']
+    # files = ['009','010','011','b','g','r','black']
+    # names = ['box','green','red','b','g','r','black']
+    files = ['009','010','011','b1','b2']
+    names = ['box','green','red','b1','b2']
     for i in range(len(files)):
         print('file: ', names[i])
         img = cv2.imread(f't:\\images\\{files[i]}.jpg')
@@ -14,10 +16,10 @@ def hsv_values():
         avg1=[]
         avg2=[]
         for j in range(3):
-            avg1.append(np.average(img[:,:,j]))
-            avg2.append(np.average(img2[:,:,j]))
-        print('bgr', avg1)
-        print('hsv',avg2)
+            avg1.append(round(np.average(img[:,:,j]),1))
+            avg2.append(round(np.average(img2[:,:,j]),1))
+        print('bgr', avg1, end='\t')
+        print('hsv',avg2, end='\t')
         print()
 
 
@@ -84,6 +86,44 @@ def get_ruler_size():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
+def get_old_ruler_size():
+    img = cv2.imread('t:\\images\\r-043.jpg')
+    img_cpy = img.copy()
+    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower = np.array([90,50,100])
+    upper = np.array([100,255,255])
+    mask = cv2.inRange(img2,lower,upper)
+
+    contours,hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # contours2 = sorted(contours, key=lambda a: cv2.contourArea(a), reverse=True)
+    contours2 = [ (cv2.contourArea(a), a) for a in contours]
+    contours2 = sorted(contours2, key = lambda a: a[0], reverse=True )
+    res = cv2.bitwise_and(img_cpy,img_cpy,mask=mask)
+    minx, maxx, miny, maxy = 9999, 0, 9999, 0
+    i = 0
+    for con in contours2:
+        area = con[0]
+        c = con[1]
+        if area < 1200: break
+        print(area)
+        cv2.drawContours(res, [c],0, (0,255,0), 3)
+        minx = min(minx, np.min(c[:,:,0]))
+        maxx = max(maxx, np.max(c[:,:,0]))
+        miny = min(miny, np.min(c[:,:,1]))
+        maxy = max(maxy, np.max(c[:,:,1]))
+        i+=1
+        if i>=5:break
+
+    ruler_height = maxy - miny
+    ruler_width = maxx - minx
+    print('ruler', ruler_height, ruler_width)
+    cv2.imshow('img', img)
+    cv2.imshow('res',res)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 def get_box_size():
     img = cv2.imread('t:\\images\\r-057.jpg')
     img_cpy = img.copy()
@@ -131,4 +171,5 @@ if __name__ == '__main__':
     # hsv_values()
     # hsv_filter()
     # get_ruler_size()
-    get_box_size()
+    get_old_ruler_size()
+    # get_box_size()
